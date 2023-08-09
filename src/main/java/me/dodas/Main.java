3ae1bdefad0277a57dafffb7e7748cf5f7bd8196
Dodas_ <ph005009004@gmail.com>
2023-08-09 01:32:43 -0300

    Initial commit

---
+++ src/main/java/me/dodas/Main.java
@@ -0,0 +1,9 @@
+package me.dodas;
+
+import me.dodas.window.home.HomeScreen;
+
+public class Main {
+    public static void main(String[] args) {
+        new HomeScreen();
+    }
+}
---
+++ src/main/java/me/dodas/window/home/HomeScreen.java
@@ -0,0 +1,23 @@
+package me.dodas.window.home;
+
+import javax.swing.*;
+
+public class HomeScreen {
+
+    public HomeScreen(){
+        JFrame jFrame = new JFrame();
+
+        jFrame.setTitle("ImageFH");
+        jFrame.setResizable(false);
+        jFrame.setSize(500, 600);
+
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
+
+        // Add component
+
+    }
+
+    
+
+}
